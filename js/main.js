92ef04ea16599ed681bddc8a3ef65563045ad678
FrankFan <fanyong@gmail.com>
2015-01-12 23:33:01 +0800

    add confirm feature

--- js/main.js
+++ js/main.js
@@ -8,29 +8,34 @@ require.config({
 require(['jquery', 'window'], function($, w){
 	$('#a').click(function(){
 		var win = new w.Window();
-		// win.alert({
-		// 	title: '提示',
-		// 	content: 'welcome!',
-		// 	width: 300,
-		// 	height: 150,
-		// 	y : 50,
-		// 	hasCloseBtn: true,
-		// 	text4AlertBtn: 'OK',
-		// 	dragHandle: '.window_header',
-		// 	// skinClassName: 'window_skin_a',
-		// 	handler4AlertBtn: function(){
-		// 		alert('you click the alert button');
-		// 	},
-		// 	handler4CloseBtn: function(){
-		// 		alert('you click the close button');
-		// 	}
-		// }).on('alert', function(){ alert('the second alert handler');});
+		win.alert({
+			title: '提示',
+			content: 'welcome!',
+			width: 300,
+			height: 150,
+			y : 50,
+			hasCloseBtn: true,
+			text4AlertBtn: 'OK',
+			dragHandle: '.window_header',
+			// skinClassName: 'window_skin_a',
+			handler4AlertBtn: function(){
+				alert('you click the alert button');
+			},
+			handler4CloseBtn: function(){
+				alert('you click the close button');
+			}
+		}).on('alert', function(){ alert('the second alert handler');});
 
 		// win.on('alert', function(){alert('you click the alert button');});
 		// win.on('alert', function(){alert('the second alert handler');});
 		// win.on('alert', function(){alert('the third alert handler');});
-		// win.on('close', function(){alert('you click the close button');});
-		// win.on('close', function(){alert('the second close handler');});
+		win.on('close', function(){alert('you click the close button');});
+		// win.on('close', function(){alert('the second close handler');});		
+	});
+
+
+	$('#b').click(function(){
+		var win = new w.Window();
 
 		win.confirm({
 			title: '系统消息',
