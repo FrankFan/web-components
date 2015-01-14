3ed85e9a872f0f712c58a9d4a23a0cf7e356befc
FrankFan <fanyong@gmail.com>
2015-01-15 00:16:14 +0800

    add js uglify task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,12 @@
 /**
  * @desc 使用grunt配置各项任务
  * @ v0.1 配置 cssmin 任务
- * 
+ * @ v0.2 配置 uglify js任务
  */
 
 module.exports = function(grunt){
+
+	pkg: grunt.file.readJSON('package.json'),
 
 	grunt.initConfig({
 		cssmin: {
@@ -20,10 +22,24 @@ module.exports = function(grunt){
 					'dest/css/main.min.css': ['css/*.css'] // 前面是输出文件, 后面是输入文件, 将后面的文件合并为一个
 				}
 			}
+		},
+
+		uglify: {
+			options: {
+				// banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' 
+				// + '<%= grunt.template.today("yyyy-mm-dd") %> */'
+			},
+			target: {
+				files: {
+					'dest/js/main.min.js': ['js/*.js', 'js/!*.jquery-1.10.2.js', 'js/!*.require.js']
+				}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	grunt.registerTask('default', ['cssmin:minify', 'cssmin:combine']);
+	grunt.registerTask('uglifyjs',  ['uglify:target']);
 };
